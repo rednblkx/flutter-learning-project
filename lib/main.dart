bcb4399654c17ec26e4348b73628b91f74641c52
White Red <rednblkx@protonmail.com>
2022-02-06 12:51:21 +0200

    removed redundant code and comments

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -40,15 +31,6 @@ class Res {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -70,16 +52,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: [
           // IconButton(onPressed: _incrementCounter, icon: icon)
@@ -88,8 +62,6 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Container(
             margin: const EdgeInsets.symmetric(horizontal: 20.0),
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
             child: DynamicTable(
                 data: _data,
                 onChanged: (el) => {
@@ -101,58 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
                         }
                     })),
       ),
-      // persistentFooterButtons: [
-      //   IconButton(
-      //     icon: const Icon(Icons.add_box),
-      //     tooltip: "Add Column",
-      //     onPressed: () {
-      //       setState(() {
-      //         columns++;
-      //       });
-      //     },
-      //   ),
-      //   IconButton(
-      //     icon: const Icon(Icons.add_box_outlined),
-      //     tooltip: "Add row",
-      //     onPressed: () {
-      //       setState(() {
-      //         rows++;
-      //       });
-      //     },
-      //   ),
-      //   IconButton(
-      //     icon: const Icon(Icons.remove_circle),
-      //     tooltip: "Remove Column",
-      //     onPressed: () {
-      //       if (columns == 1) {
-      //         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      //           content: Text("Cannot remove last column"),
-      //           duration: Duration(seconds: 2),
-      //         ));
-      //       } else {
-      //         setState(() {
-      //           columns--;
-      //         });
-      //       }
-      //     },
-      //   ),
-      //   IconButton(
-      //     icon: const Icon(Icons.remove_circle_outline),
-      //     tooltip: "Remove row",
-      //     onPressed: () {
-      //       if (rows == 1) {
-      //         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      //           content: Text("Cannot remove last row"),
-      //           duration: Duration(seconds: 2),
-      //         ));
-      //       } else {
-      //         setState(() {
-      //           rows--;
-      //         });
-      //       }
-      //     },
-      //   ),
-      // ],
       drawer: const Drawer(
         child: Text("gfdgdfg"),
       ), // This trailing comma makes auto-formatting nicer for build methods.
@@ -206,12 +126,7 @@ class DynamicColumn extends StatelessWidget {
   }
 
   _navigateAndDisplaySelection(BuildContext context) async {
-    // Navigator.push returns a Future that completes after calling
-    // Navigator.pop on the Selection Screen.
-    final result = await Navigator.push(
-        context,
-        // Create the SelectionScreen in the next step.
-        _dialogBuilder(context));
+    final result = await Navigator.push(context, _dialogBuilder(context));
     // return result;
     // ScaffoldMessenger.of(context)
     //   ..removeCurrentSnackBar()
